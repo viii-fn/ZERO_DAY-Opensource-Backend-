d70b69718cbe80a53ab0d0ed0c36385088470fc2
viii-fn <irhayeelvisebah@gmail.com>
2026-09-01 21:24:08 +0100

    Cleaned up post model

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 public class PostModel
 {
 	[Required]
 	[StringLength(1000, ErrorMessage = "Post exceeds the 400 character limit.")]
-	public string Post { get; set; }
-	[EmailAdress]
-	public string Email { get; set; }
+	public string? Post { get; set; }
+	[EmailAddress]
+	public string? Email { get; set; }
 }
