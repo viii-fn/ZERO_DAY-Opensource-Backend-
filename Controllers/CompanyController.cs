a59972f33189ac460b2cfac46427b80202e5d6b6
viii-fn <irhayeelvisebah@gmail.com>
2026-09-03 14:06:23 +0100

    ##

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -7,7 +7,7 @@ namespace ZERO_DAY.Controllers;
 public class CompanyController : ControllerBase
 {
 	[HttpGet]
-	public AcrionResult<string> GetCompany()
+	public ActionResult<string> GetCompany()
 	{
 		return new string[] { "Vidpixar", "SerzaMoon", "Zero Day", "Paramedy" };
 	}
